d2a5ac1440d4a273d4205bc41e4cf8fbdfcc7b47
Wyatt Ades <wyattades@gmail.com>
2021-12-28 13:26:36 -0800

    add umami

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import React from "react";
+import Head from "next/head";
+import Script from "next/script";
 import type { AppProps } from "next/app";
 
 import "styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Logic Gates</title>
+        <meta
+          name="description"
+          content="Create and simulate Logic Gates in the web"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {process.env.NODE_ENV === "production" && (
+        <Script
+          data-website-id="4df3a72a-387e-4526-ab91-2a83c23aceeb"
+          src="https://sip-umami.vercel.app/umami.js"
+        />
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,10 @@
 import React from "react";
-import Head from "next/head";
 
 import { LogicGates } from "components/LogicGates";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col items-stretch">
-      <Head>
-        <title>Logic Gates</title>
-        <meta
-          name="description"
-          content="Create and simulate Logic Gates in the web"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex flex-col items-stretch flex-1">
         <h1 className="p-6 text-xl font-bold">Logic Gates</h1>
 
